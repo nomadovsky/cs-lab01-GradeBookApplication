6f6e56e06a7678f8ef11e7e2d44a479e5934b808
nomadovsky <nomadovsky@gmail.com>
2022-03-03 23:35:08 +0100

    Refactor constructor of BaseGradeBook

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,19 +34,20 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
+            bool isWeighted = parts[3]=="true";
             if (parts[2] == "standard") {
-                StandardGradeBook gradeBook = new StandardGradeBook(name);
+                StandardGradeBook gradeBook = new StandardGradeBook(name, isWeighted);
                 GradeBookUserInterface.CommandLoop(gradeBook);
             }
             else if (parts[2] == "ranked")
             {
-            RankedGradeBook gradeBook = new RankedGradeBook(name);
+            RankedGradeBook gradeBook = new RankedGradeBook(name, isWeighted);
                 GradeBookUserInterface.CommandLoop(gradeBook);
             }
             else Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
